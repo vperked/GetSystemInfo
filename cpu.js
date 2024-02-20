b537eff3a9e098f0b5e1583910839dda8ed23f9d
vperked <perked@riseup.net>
2024-02-20 18:56:52 -0500

    Adding stuff.

--- cpu.js
+++ cpu.js
@@ -1,42 +1,71 @@
 const si = require('systeminformation')
 const readline = require ('readline');
+const { clear } = require('console');
 
+/// Welcome Message!
+console.log("Welcome to the CPU section of GSI!")
+
+/// Create Readline Interface
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 })
 
-console.log("Welcome to the CPU section of GSI!")
-
-
-
 /// Main Functions!
 
-function displayCPUSpeed() {
-si.cpuCurrentSpeed()
-.then (data => console.log(data))
-.catch (error => console.error(error))
+function displayCPUCache () {
+    si.cpuCache()
+    .then (data => console.log(data))
+    .catch (error => console.error(error));
+}
+
+    function displayCPUSpeed() {
+    si.cpuCurrentSpeed()
+    .then (data => console.log(data))
+    .catch (error => console.error(error))
+    homeScreen()
 }
 
 function displayCPUinfo() {
-si.cpu()
-.then (data => console.log(data))
-.catch (error => console.error(error));
+    si.cpu()
+    .then (data => console.log(data))
+    .catch (error => console.error(error));
 }
 
+function CPUspeedMessage () {
+    console.log ("Getting your CPU Speed...");
+}
+
+function CPUInfoMessage () {
+    console.log ("Getting your CPU Info...");
+}
+
+function CPUCacheInfoMessage () {
+    console.log ("Getting your CPU Cache...")
+} 
+
+
+/// Getting your input.
 
 function getUserInput() {
-    rl.question("Press 1 to display CPU current speed, Press 2 to display CPU information: ", (userInput) => {
+    rl.question("Press 1 to bring up your CPU Speed, Press 2 to bring up info about your CPU, Press 3 to bring up your current CPU cache. ", (userInput) => {
+        clear()
         switch (userInput.trim()) {
             case '1':
-                displayCPUSpeed();
+                CPUspeedMessage()
+                displayCPUSpeed()
                 break;
             case '2':
+                CPUInfoMessage()
                 displayCPUinfo();
                 break;
+                case '3':
+                CPUCacheInfoMessage() 
+                displayCPUCache();
+                break;
             default:
-                console.log("Invalid input. Please press 1 or 2.");
+                console.log("Invalid input. Please press 1,2 or 3.");
         }
 
         rl.close();
@@ -44,3 +73,5 @@ function getUserInput() {
 }
 
 getUserInput();
+
+/// End
---
+++ mem.js
@@ -0,0 +1,52 @@
+const si = require('systeminformation')
+const readline = require ('readline');
+const { clear } = require('console');
+
+
+/// Create Readline Interface
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+/// Main Functions
+
+function displayTotalMem () {
+    si.mem()
+    .then (data => console.log(data))
+    .catch (error => console.error(error));
+}
+
+
+function memInfo () {
+    si.memLayout()
+    .then (data => console.log(data)) 
+    .catch (error => console.error(error));
+}
+
+
+function totalMemInfo () {
+    console.log ("Getting your Memory Info...")
+}
+
+/// Getting your input.
+
+function getUserInput() {
+    rl.question("Press 1 to Display your Total Memory or Press 2 to display Info about your Memory:", (userInput) => {
+        clear()
+        switch (userInput.trim()) {
+            case '1':
+                totalMemInfo()
+                displayTotalMem();
+                break
+            case '2':
+            memInfo();
+            break
+            default:
+                console.log("Invalid input. Please press 1 or 2.");
+        }
+        rl.close();
+    });
+}
+
+getUserInput();
