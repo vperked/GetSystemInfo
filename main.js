76f49b334870a996d67b195979f2bdbda6cd96cd
vperked <perked@riseup.net>
2024-02-22 11:26:48 -0500

    Added ping command in main.js.

--- main.js
+++ main.js
@@ -22,9 +22,22 @@ const rl = readline.createInterface({
 function helloMessage() {
     console.log(chalk.blue.bold("Welcome to GSI! This is the welcome page!"));
     console.log(chalk.red("This project was made to improve my backend skills, in the commands folder is where all the active functions are."))
+    console.log(chalk.green.bold("This file has basic things, input numbers 1,2 or 3 to find out what hidden features there are."))
 }
 
+function keepGoing () {
+    setInterval(() => {
+    }, 1000)
+}
 
+function ping () {
+    si.inetLatency('1.1.1.1')
+    .then (data => console.log (data));
+}
+
+function pingMessage () {
+    console.log (chalk.yellow("Getting your latency to 1.1.1.1..."))
+}
 
 /// Getting User Input
 
@@ -34,8 +47,9 @@ function getUserInput() {
         clear()
         switch (userInput.trim()) {
             case '1':
-                totalMemInfo()
-                displayTotalMem();
+                pingMessage()
+                ping()
+                keepGoing()
                 break
             default:
                 console.log("Invalid input. Please press 1 or 2.");
